06b7f1a472e76dbfef9067fe91afaa68f0dd8c0c
John Kriz <janoskrizsan402@gmail.com>
2020-03-12 12:56:57 +0100

    object creation, xml serialization

--- Data_Provider/Data_Provider/static/utils.py
+++ Data_Provider/Data_Provider/static/utils.py
@@ -1,7 +1,9 @@
 from Data_Provider.Models.author import Author
 from Data_Provider.Models.article import Article
 from Data_Provider.Models.source import Source
-
+import datetime
+import xml.etree.ElementTree as et
+import io
 
 def create_val_string(values):
 	string_vals = []
@@ -13,20 +15,53 @@ def create_val_string(values):
 			string_vals.append("")
 	return "".join(string_vals)
 
+
+def set_attributes(obj, data):
+	for k, v in data.iteritems():
+		setattr(obj, k, v)
+	return obj
+
 def create_author(data):
 	author = Author()
-	return author
+	return set_attributes(author, data)
 
 def create_article(data):
 	article = Article()
-	return article
+	return set_attributes(article, data)
 
 def create_source(data):
 	source = Source()
-	return source
+	return set_attributes(source, data)
 
-def serialize_to_file():
-	pass
+def serialize_to_file(article):
+	data = et.Element("article")
+	title = et.SubElement(data, "title")
+	sub_title = et.SubElement(data, "sub_title")
+	content = et.SubElement(data, "content")
+	author = et.SubElement(data, "author")
+	creation_date = et.SubElement(data, "creation_date")
+	sources = et.SubElement(data, "sources")
+	
+	title.text(f"{article.prop_name}")
+	sub_title.text(f"{article.prop_subtitle}")
+	content.text(f"{article.prop_content}")
+	author.text(f"{article.prop_author}")
+	creation_date.text(f"{article.prop_creationdate}")
+	sources.text(f"{article.prop_sources}")
+	
+	article_data = et.tostring(data)
+	with open(f"{article.prop_fullfilepath}", "w") as article_file:
+		article_file.write(article_data)
 
-def read_from_file():
-	pass
+
+def read_from_file(file_path):
+	data = et.parse(file_path)
+	n = None
+	article_info = {"prop_name" : data.findtext("title", n),
+					"prop_subtitle" : data.findtext("sub_title", n),
+					"prop_content" : data.findtext("content", n),
+					"prop_author" : data.findtext("author", n),
+					"prop_creationdate" : datetime.strptime(data.findtext("creation_date", n), "%m-%d-%Y").date(),
+					"prop_sources" : data.findtext("sources", n)
+					}
+	return create_article(article_info)
